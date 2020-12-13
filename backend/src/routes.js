68a1e257a1fe5910fdbf46bb1c3a72df4eec773d
PedroGuarany <pedrogt.alu.lmb@gmail.com>
2020-12-13 14:07:44 -0300

    Configuracao servidor

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,1 @@
 import {Router} from 'express';
-
-
-
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333);
+app.get('/users', (request, response) => {
+    return response.json({message: 'Hello World!'})
+})
+
+app.listen(3333);  
