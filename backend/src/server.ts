eb8ae66317d2c8b444edc65a13ff195c005250c0
PedroGuarany <pedrogt.alu.lmb@gmail.com>
2020-12-13 15:14:21 -0300

    Função de cadastrar usuários criada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import User from './models/User'
 
 import './database/connection';
 
@@ -6,8 +8,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-    return response.json({message: 'Hello World!'});
-})
+app.post('/users', async (request, response) => {
+    const {name, acess_code} = request.body;
+    
+    const userRepository = getRepository(User)
+    
+    const user = userRepository.create({
+        name,
+        acess_code,
+    });
+    
+    await userRepository.save(user);
+
+    return response.json({message: 'Usuário cadastrado!'});
+});
 
 app.listen(3333);
